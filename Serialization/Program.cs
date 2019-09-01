1de39f6e3b3b7dd78c449e714d211958f4773601
Dongyeon Lee <dlee177@my.centennialcollege.ca>
2019-09-01 04:21:59 -0400

    Serialization

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace Serialization
 {
@@ -10,6 +9,39 @@ namespace Serialization
     {
         static void Main(string[] args)
         {
+            List<Atom> element = Atom.GetAtoms();
+            SerializeAllAtoms("allAtoms.json", element);
+            DeserializeAllAtoms("allAtoms.json");
+        }
+
+        // Serialization Method
+        static void SerializeAllAtoms(string filename, List<Atom> element)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            using (TextWriter writer = new StreamWriter(filename))
+            {
+                string output = serializer.Serialize(element);
+                writer.Write(output);
+            }
+        }
+
+        // Deserialization Method
+        static void DeserializeAllAtoms(string filename)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            using (TextReader reader = new StreamReader(filename))
+            {
+                string input = reader.ReadToEnd();
+                List<Atom> atoms = serializer.Deserialize<List<Atom>>(input);
+                int count = 1;
+
+                foreach (Atom element in atoms)
+                {
+                    Console.WriteLine($"{count++,-2} - {element}");
+                }
+            }
         }
     }
 }
